a24a80c9ff8aa0ee67d56b9ba986e1f0d00ba602
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-09-24 21:14:11 +0500

    Task1

---
+++ Tyuiu.BocharovaES.Sprint1.Task1.V29/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.BocharovaES.Sprint1.Task1.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Бочарова Е. С. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Организация ввода/вывода в консольный приложениях                 *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнила: Бочарова Екатерина Сергеевна | ИИПб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ :                                                               *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле a*b*c/(a+b+c) и печатает его на экране.  *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        double a, b, c;
+
+        Console.WriteLine("Введите значение A:");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение B:");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение C:");
+        c = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(a,b,c));
+
+        Console.ReadLine();
+    }
+}
