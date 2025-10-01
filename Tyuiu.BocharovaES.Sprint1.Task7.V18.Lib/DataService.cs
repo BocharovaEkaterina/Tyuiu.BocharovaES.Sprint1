8b21dad5cbca3d8654eac08b3e864dd61ac82cce
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-10-01 21:51:29 +0500

    Task7

---
+++ Tyuiu.BocharovaES.Sprint1.Task7.V18.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BocharovaES.Sprint1.Task7.V18.Lib
+{
+    public class DataService : ISprint1Task7V18
+    {
+        public double Calculate(double x, double y)
+        {
+            double f = ((1+Math.Pow((Math.Sin(x+y)),2))/(2+Math.Abs(x-(2*x/(1+x*x*y*y))))) + x;
+            return Math.Round(f,3);
+        }
+    }
+}
